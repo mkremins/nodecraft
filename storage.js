2179033bc5a4c375c00116b52d27ba0f9275655e
Max Kreminski <maxkreminski@gmail.com>
2013-07-23 13:54:31 -0400

    Cache created DataStore objects for improved performance

--- storage.js
+++ storage.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
-
-function makeFilepath(dirpath, key) {
-	return dirpath + '/' + key + '.json';
-}
+var stores = {};
 
 function DataStore(name) {
 	this._dirpath = __dirname + '/' + name;
 }
 
+DataStore.prototype._makeFilepath = function(key) {
+	return this._dirpath + '/' + key + '.json';
+};
+
 DataStore.prototype.get = function(key, callback) {
-	var filepath = makeFilepath(this._dirpath, key);
-	fs.readFile(filepath, 'utf8', function(err, data) {
+	fs.readFile(this._makeFilepath(key), 'utf8', function(err, data) {
 		if (err) {
 			throw err; // TODO actually handle the error
 		} else {
@@ -20,23 +20,24 @@ DataStore.prototype.get = function(key, callback) {
 };
 
 DataStore.prototype.set = function(key, value) {
-	var filepath = makeFilepath(this._dirpath, key);
-	fs.writeFile(filepath, JSON.stringify(value), function(err) {
+	fs.writeFile(this._makeFilepath(key), JSON.stringify(value), function(err) {
 		if (err) {
 			throw err; // TODO actually handle the error
 		}
 	});
 };
 
-function load(type, key, callback) {
-	new DataStore(type).get(key, callback);
-}
-
-function save(type, key, value) {
-	new DataStore(type).set(key, value);
+function getDataStore(type) {
+	stores[type] = stores[type] || new DataStore(type);
+	return stores[type];
 }
 
 module.exports = {
-	save: save,
-	load: load
+	load: function(type, key, callback) {
+		getDataStore(type).get(key, callback);
+	},
+
+	save: function(type, key, value) {
+		getDataStore(type).set(key, value);
+	}
 };
